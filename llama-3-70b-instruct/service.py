00e549e6caacaafd6a8eadcebef3b54bf27f8703
Rick Zhou <rickzhoucmu@gmail.com>
2024-06-01 00:15:33 +0000

    Use TGI base_image. Avoid running docker inside docker

--- llama-3-70b-instruct/service.py
+++ llama-3-70b-instruct/service.py
@@ -2,9 +2,8 @@ from typing import AsyncGenerator, Optional
 
 import bentoml
 from annotated_types import Ge, Le
-from typing_extensions import Annotated
 from openai import AsyncOpenAI
-
+from typing_extensions import Annotated
 
 MAX_TOKENS = 1024
 SYSTEM_PROMPT = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
@@ -31,21 +30,11 @@ class TGI:
     bento_model_ref = bentoml.models.get(BENTO_MODEL_TAG)
 
     def __init__(self) -> None:
-        import docker
+        import subprocess
 
-        client = docker.from_env()
         target_dir = self.bento_model_ref.path
-        container = client.containers.run(
-            image="ghcr.io/huggingface/text-generation-inference:2.0",
-            runtime="nvidia",
-            shm_size="1g",
-            volumes={target_dir: {"bind": "/models", "mode": "ro"}},
-            ports={"80/tcp": 8080},
-            command=f"--model-id /models --quantize awq",
-            detach=True,
-        )
-        print(container.logs())
-        print("Container started with ID:", container.id)
+        command = f"text-generation-launcher --model-id {target_dir} -p 8080"
+        subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 
         self.client = AsyncOpenAI(
             base_url="http://localhost:8080/v1", api_key="not needed for a local LLM"
@@ -58,7 +47,9 @@ class TGI:
         system_prompt: Optional[str] = SYSTEM_PROMPT,
         max_tokens: Annotated[int, Ge(128), Le(MAX_TOKENS)] = MAX_TOKENS,
     ) -> AsyncGenerator[str, None]:
-        messages = [{"role": "system", "content": system_prompt}] if system_prompt else []
+        messages = (
+            [{"role": "system", "content": system_prompt}] if system_prompt else []
+        )
         messages.append({"role": "user", "content": prompt})
         stream = await self.client.chat.completions.create(
             model="tgi",
